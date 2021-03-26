706df7cf668c2b7b3ff64243b3601162682b0eb9
joker-007x <69024702+joker-007x@users.noreply.github.com>
2021-03-26 07:00:47 +0530

    spprofile chat

--- spProfile/action.php
+++ spProfile/action.php
@@ -3,28 +3,33 @@
     session_start();
     $sql_getusertype = mysqli_query($con,"SELECT type FROM user WHERE id = '".$_SESSION['user_id']."' ");
     $usertype=mysqli_fetch_assoc($sql_getusertype);
-    $sql_getusername = mysqli_query($con,"SELECT firstname from '".$usertype["type"]."' where id = '".$_SESSION['user_id']."' ");
+    // $tablename = "";
+    // if($usertype['type'] == "eo")
+    //     $tablename = "event_organizer";
+    // else if($usertype['type'] == "sp")
+    $sql_getusername = mysqli_query($con,"SELECT firstname from event_organizer where id = '".$_SESSION['user_id']."' ");
     $getname = mysqli_fetch_assoc($sql_getusername);
     mysqli_query($con,"INSERT INTO message (reciever_userid, sender_userid, message, status) values('".$_SESSION['spid']."', '".$_SESSION['user_id']."', null, '1') ");
-    $sql_checkuser = mysqli_query($con,"SELECT userid from chat_users");
+    $sql_checkuser = mysqli_query($con,"SELECT * from chat_users");
     // $checkuser = mysqli_fetch_assoc($sql_checkuser);
     $flags = 0;
     while ($checkuser = $sql_checkuser->fetch_assoc())
     {
-        if($checkuser["userid"] == $_SESSION['user_id']){
+        if($checkuser['userid'] == $_SESSION['user_id']){
             $flags = 1;
         }
     }
     $flagr = 0;
     while ($checkuser = $sql_checkuser->fetch_assoc())
     {
-        if($checkuser["userid"] == $_SESSION['spid']){
+        if($checkuser['userid'] == $_SESSION['spid']){
             $flagr = 1;
         }
     }
-    if(!$flags)
+    if($flags == 0)
         mysqli_query($con,"INSERT INTO chat_users (userid, username, current_session) values('".$_SESSION['user_id']."', '".$getname['firstname']."', '".$_SESSION['spid']."') ");
-    if(!$flagr)
-        mysqli_query($con,"INSERT INTO chat_users (userid, username, current_session) values('".$getname['firstname']."', '".$_SESSION['user_id']."', '".$_SESSION['spid']."') ");
-    header('Location: ../message/index.php');
+    echo $getname['firstname'];
+    if($flagr == 0)
+        mysqli_query($con,"INSERT INTO chat_users (userid, username, current_session) values('".$_SESSION['spid']."', '".$getname['firstname']."', '".$_SESSION['user_id']."') ");
+    // header('Location: ../message/index.php');
 ?>
