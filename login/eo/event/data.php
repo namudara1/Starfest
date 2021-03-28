46e628b7adce0dff5364007994a08232fe7821e1
joker-007x <69024702+joker-007x@users.noreply.github.com>
2021-03-28 21:40:43 +0530

    minor changes

--- login/eo/event/data.php
+++ login/eo/event/data.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once "../../../config/connection.php";
+
 session_start();
 //echo "<pre>", print_r($_FILES['pro']['name']),"</pre>";
 
@@ -59,6 +61,7 @@ $eo_idd = $row['eo_id'];
 $sql = "UPDATE event SET event_name='$event_name',category='$category', type='$type',date='$date', time='$time', participant_amt='$participant_amt',description='$description', venue= '$venue',image='$profileImage'  where event_id='$e1_id'";
 
 
+
 // $sql = "UPDATE event SET event_name='',,,,,,, ,, eo_id)
 // values (,,,,,,,,,'$eo_idd')";
 
@@ -70,6 +73,10 @@ $sql = "UPDATE event SET event_name='$event_name',category='$category', type='$t
 if ($conn->query($sql)){
 // echo "New record is inserted sucessfully";
 // echo "user id: {$_SESSION['user_id']}<br>";
+$sql_getSp=mysqli_query($con,"SELECT * FROM event_request WHERE event_id = '".$e1_id."' and status = 'accepted'");
+while ($getsps = $sql_getSp->fetch_assoc()){
+    mysqli_query($con,"INSERT INTO event_request (eo_userid, sp_userid, event_id) values('".$_SESSION['user_id']."', '".$getsps['sp_userid']."', '".$e1_id."') ");
+}
 header('Location: ../index.php');
 }
 else{
