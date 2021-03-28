c215567bea123674f2ba064e8bf1538f77062b36
Jananigamage <Starkrizy.jani@gmail.com>
2021-03-28 22:48:39 +0530

    updated

--- login/sp/index.php
+++ login/sp/index.php
@@ -21,7 +21,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>W3.CSS Template</title>
+<title>Service Provider Dashbord</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="../css/style.css">
