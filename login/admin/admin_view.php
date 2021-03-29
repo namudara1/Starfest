7a368d0ee116d00d237c8344716f1a1c11ff665e
1996-isuru <isurusrimalnt@gmail.com>
2021-03-30 02:44:08 +0530

    admin part

---
+++ login/admin/admin_view.php
@@ -0,0 +1,101 @@
+<?php
+require_once('connection.php'); 
+
+
+// if($_GET['check_id'] == 1){
+//     $check_id = $_GET['check_id'];
+//     // $dd = '2020-11-13';
+//     $dd = date("Y-m-d");
+//     $sql = "SELECT * FROM event where type='public' AND date < '{$dd}'";
+//     $result = mysqli_query($connection,$sql) ;
+// }
+// else{
+//     $check_id = $_GET['check_id'];
+//     // $dd = '2020-11-13';
+//     $dd = date("Y-m-d");
+//     $sql = "SELECT * FROM event where type='public' AND date > '{$dd}'";
+//     $result = mysqli_query($connection,$sql) ;
+// }
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="">
+<head>
+<title>Starfest</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+
+<link href="layout/styles/work.css" rel="stylesheet" type="text/css" media="all">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link href="layout/styles/mainwork.css" rel="stylesheet" type="text/css" media="all">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body id="top">
+
+<div class="wrapper row0">
+  <div id="topbar" class="hoc clear"> 
+  </div>
+</div>
+<div class="wrapper row1"> 
+</div>
+
+    <br><br><br>
+    <div class="sectiontitle">
+
+    <?php
+    if($_GET['check_id'] == 1){
+    $check_id = $_GET['check_id'];
+    // $dd = '2020-11-13';
+    $dd = date("Y-m-d");
+    $sql = "SELECT * FROM event where type='public' AND date < '{$dd}'";
+    $result = mysqli_query($connection,$sql) ;
+    echo '<b> <h1 class="logoname"><span>Finished Events</span></h1>';
+    }
+    else{
+    $check_id = $_GET['check_id'];
+    // $dd = '2020-11-13';
+    $dd = date("Y-m-d");
+    $sql = "SELECT * FROM event where type='public' AND date > '{$dd}'";
+    $result = mysqli_query($connection,$sql) ;
+    echo '<b> <h1 class="logoname"><span>Up Comming Events</span></h1>';
+    }
+    ?>
+
+<ul class="nospace group overview">
+  
+<?php 
+while($row=mysqli_fetch_assoc($result)){
+
+?> 
+<div class="w3-third w3-margin-bottom" >
+      <ul class="w3-ul w3-border w3-hover-shadow">
+        <li class="w3-theme-l2">
+          <p class="w3-xlarge"><a href="view.php?id=<?=$row['event_id']; ?>">
+   <h6 class="heading"><?php echo $row["event_name"]; ?></h6>
+       <?php echo "<li><img src='../../create_event/upload/{$row['image']}' alt='{$row['event_name']}' class='gallery'></li>"; ?>
+</a></p>
+        </li>
+        <li class="w3-padding-16"><p><?php echo $row["description"]; ?></p></li>
+        <li><footer><a class="btn" href="report/index.php?id=<?=$row['event_id']; ?>">TICKET DETAILS</a></footer></li>
+      
+      </ul>
+</div>
+ <?php
+}
+
+?>
+
+</ul>
+
+</div>
+
+<a id="backtotop" href="#top"><i class="fas fa-chevron-up"></i></a>
+</body>
+</html>
+<?php
+    mysqli_close($connection);
+?>
---
+++ login/admin/search_data.php
@@ -0,0 +1,70 @@
+<?php 
+    require_once('connection.php'); 
+    session_start();
+    $date = $_POST['date'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+   
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    
+    <div class="suc">
+        <h2 style="font-size:400%;"><?php echo ($date. "  Events") ?></h2>
+
+        <table id = "admin_date_table">
+                <tr>
+                    <th><h2 class="text1">Event Name</h2></th>
+                    <th><h2 class="text1"></h2></th>
+                </tr>
+
+             
+            <?php
+             $query = "SELECT * FROM event WHERE DATE(date) = '$date' AND type = 'public'";
+             
+             $result_set = mysqli_query($connection, $query);
+
+             if($result_set){
+                 while ($record = mysqli_fetch_assoc($result_set)){
+                     $name = $record ['event_name'];
+                     $event_id = $record ['event_id'];
+                    
+                     echo '<tr>';
+                        echo '<td>';
+                            echo $name;
+                        echo '</td>';
+                        echo '<td>';
+                            echo '<div class="cart_btn">';
+                            echo '<a href="report/index.php?id='.$event_id.'"><button id="redirect_button" class="redirect">Ticket Details</button></a>';
+                            echo '</div>';
+                        echo '</td>';
+                    echo '</tr>';
+                }
+            }
+            else{
+                echo '<tr>';
+                        echo '<td>';
+                            echo "No Events.";
+                        echo '</td>';
+                        echo '<td>';
+                            echo " ";
+                        echo '</td>';
+                echo '</tr>';
+
+            }
+            ?>
+            </table>
+    </div>
+</body>
+</html>
+
+<?php
+    mysqli_close($connection);
+?>
